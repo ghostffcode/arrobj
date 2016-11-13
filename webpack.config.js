8a2ad9644d5e55ab15ee8754ff683e08b550e192
PoeticBliss <meremezeukah@gmail.com>
2016-11-12 22:42:10 -0500

    updated buuild config

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     library: process.env.NAME,
     libraryTarget: process.env.TARGET,
     path: __dirname,
-    filename: (PROD) ? 'build/ghost.min.js' : 'build/ghost.js'
+    filename: (PROD) ? `build/${process.env.NAME}.min.js` : `build/${process.env.NAME}.js`
   },
   module: {
     loaders: [
